d64d76ce6a2a41d6351a4d9fdade29cb0cd5c417
Um seoyoung <tjdud0123@naver.com>
2020-10-17 23:34:07 +0900

    [ 실습 ] 사용자 이름 출력

--- 실습/2nd-seminar-git/script.js
+++ 실습/2nd-seminar-git/script.js
@@ -1,1 +1,29 @@
 const BASE_URI = "https://api.github.com/users/"
+
+const $main = document.getElementById("main"); 
+const $form = document.getElementById("form");
+const $input = document.getElementById("input");
+
+const getUser = async (userName) => {
+    const response = await fetch(`${BASE_URI}${userName}`);
+    const userData = await response.json();
+    createUserCard(userData);
+}
+
+const createUserCard = (userData) => {
+    const cardHTML =  `<h2>${userData.name}</h2>`;
+    $main.innerHTML = cardHTML;
+}
+
+$form.addEventListener("submit", (e) =>{
+    e.preventDefault(); // 새로고침 방지
+    const user = $input.value;
+    if(user) {
+        getUser(user);
+        resetForm()
+    }
+
+
+})
+
+const resetForm = () => $input.value = ''
