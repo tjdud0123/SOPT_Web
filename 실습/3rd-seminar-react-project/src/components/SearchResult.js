d8d88ec959aef882a2809e0d5088f1664d2e82c7
Um seoyoung <tjdud0123@naver.com>
2020-11-06 16:22:04 +0900

    결과 출력 정보 추가

--- 실습/3rd-seminar-react-project/src/components/SearchResult.js
+++ 실습/3rd-seminar-react-project/src/components/SearchResult.js
@@ -1,7 +1,28 @@
 import React from 'react';
 
 function SearchResult({ user }) {
-  return <div>{user && user.name}</div>;
+  return (
+    user && (
+      <div>
+        <img src={user.avatar_url} alt={user.name} />
+        <div>
+          <h2>{user.name}</h2>
+          <p>{user.bio}</p>
+          <ul>
+            <li>
+              <strong>Followers</strong> {user.followers}
+            </li>
+            <li>
+              <strong>Following</strong> {user.following}
+            </li>
+            <li>
+              <strong>Repos</strong> {user.public_repos}
+            </li>
+          </ul>
+        </div>
+      </div>
+    )
+  );
 }
 
 export default SearchResult;
