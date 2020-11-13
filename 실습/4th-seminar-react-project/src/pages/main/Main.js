b26e1f780c0f9a43f3d17653abb8572f40c797cd
Um seoyoung <tjdud0123@naver.com>
2020-11-14 01:45:49 +0900

    # [ 스터디 - redux ] - Main 페이지에서 잘 저장되어있는지 보기

--- 실습/4th-seminar-react-project/src/pages/main/Main.js
+++ 실습/4th-seminar-react-project/src/pages/main/Main.js
@@ -1,4 +1,10 @@
+// 스토에어서 불러오기
+import { useSelector } from 'react-redux';
+
 function Main() {
+  // 스토어
+  const { members } = useSelector(state => state.member);
+  console.log('saved members : ', members);
   return <h1> 메인 페이지 </h1>;
 }
 
