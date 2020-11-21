b261e0e97883061aa2cb8e11cc1768c8d4a0bf8f
Um seoyoung <tjdud0123@naver.com>
2020-11-22 01:36:04 +0900

    # 5th - update, create, delete api 생성

--- 실습 및 과제/4th-seminar-react-project/src/lib/api/memberApi.js
+++ 실습 및 과제/4th-seminar-react-project/src/lib/api/memberApi.js
@@ -1,10 +1,10 @@
 import axios from 'axios';
-const getMemberUrl =
+const MemberUrl =
   'http://ec2-13-124-127-8.ap-northeast-2.compute.amazonaws.com:3000/api/members';
 
 const getMembers = async () => {
   try {
-    const { data } = await axios.get(`${getMemberUrl}`);
+    const { data } = await axios.get(`${MemberUrl}`);
     console.log('[SUCCESS] GET MEMBERS', data);
     return data;
   } catch (e) {
@@ -14,7 +14,7 @@ const getMembers = async () => {
 
 const getMemberById = async id => {
   try {
-    const { data } = await axios.get(`${getMemberUrl}/${id}`);
+    const { data } = await axios.get(`${MemberUrl}/${id}`);
     console.log('[SUCCESS] GET MEMBER', data);
     return data;
   } catch (e) {
@@ -23,4 +23,43 @@ const getMemberById = async id => {
   }
 };
 
-export { getMembers, getMemberById };
+const updateMemberById = async (id, member) => {
+  try {
+    const { data } = await axios.put(`${MemberUrl}/${id}`, member);
+    console.log('[SUCCESS] UPDATE MEMBER', data);
+    return data;
+  } catch (e) {
+    console.error('[FAIL] UPDATE MEMBER', e);
+    throw e;
+  }
+};
+
+const createMember = async member => {
+  try {
+    const { data } = await axios.post(`${MemberUrl}`, member);
+    console.log('[SUCCESS] CREATE MEMBER', data);
+    return data.data;
+  } catch (e) {
+    console.log('[FAIL] CREATE MEMBER', e);
+    throw e;
+  }
+};
+
+const deleteMemberById = async id => {
+  try {
+    const { data } = await axios.delete(`${MemberUrl}/${id}`);
+    console.log('[SUCCESS] DELETE MEMBER', data);
+    return data.data;
+  } catch (e) {
+    console.log('[FAIL] DELETE MEMBER');
+    throw e;
+  }
+};
+
+export {
+  getMembers,
+  getMemberById,
+  updateMemberById,
+  createMember,
+  deleteMemberById,
+};
